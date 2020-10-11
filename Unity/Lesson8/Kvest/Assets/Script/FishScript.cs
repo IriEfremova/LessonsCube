4c4dc159a2806b200862093ebf5be0a547ee6c22
IriEfremova <iriefremova@gmail.com>
2020-10-12 00:16:41 +0500

    Edit

---
+++ Unity/Lesson8/Kvest/Assets/Script/FishScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishScript : MonoBehaviour
+{
+    public int speed;
+    public Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall"))
+        {
+            transform.position = new Vector3(startPos.x, startPos.y, 0);
+        }
+    }
+}
---
+++ Unity/Lesson8/Kvest/Assets/Script/PlayerScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.U2D;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject jump_animation;
+    public int speedH;
+    public int speedV;
+    bool isJump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dH = Input.GetAxis("Horizontal");
+        float dV = Input.GetAxis("Vertical");
+
+        /*
+        if(dV != 0 && isJump == false)
+        {
+            isJump = true;
+            Instantiate(jump_animation, transform.position, transform.rotation);
+            //Destroy(gameObject);
+        }
+        */
+        if (dV > 0 && isJump == false)
+        {
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, speedV));
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speedV );
+            isJump = true;
+            Debug.Log("Jump");
+        }
+        if(dH != 0 && isJump == false)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(dH * speedH, 0);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fish") || collision.gameObject.CompareTag("Land"))
+        {
+            collision.isTrigger = false;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Land"))
+        {
+            isJump = false;
+        }
+        if (collision.gameObject.CompareTag("Fish")) {
+            transform.parent = collision.transform;
+            isJump = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fish"))
+        {
+            transform.parent = null;
+        }
+    }
+}
