dcc7fd22f9a98ef26312ecdfd7e9a57184fc0f5d
IriEfremova <iriefremova@gmail.com>
2020-11-09 00:20:17 +0500

    Edit

---
+++ Unity/Lesson8/Mouse/Assets/BombScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BombScript : MonoBehaviour
+{
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown Player" + Input.mousePosition.x + "  " + Input.mousePosition.y);
+        offset = gameObject.transform.position -
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+    }
+}
